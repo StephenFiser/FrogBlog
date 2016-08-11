af2a9266bfc064854eb0bf8a42a8db56c65e066d
Stephen Fiser <stephen.fiser@gmail.com>
2016-08-11 11:47:03 -0400

    wip

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
+
   root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
